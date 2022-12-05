4069108759e39d128c38b6221ffa3992329a40d0
Gorik <gosha_qwerty@rambler.ru>
2022-12-05 16:19:28 +0800

    До делал Task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,19 @@
 ﻿//Task2
-Console.Clear();
-Console.Write("Введите первое число ");
+ Console.Clear();
+Console.Write("Введите первое число");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число ");
+Console.Write("Введите второе число");
 int m = Convert.ToInt32(Console.ReadLine());
+
+if (n > m)
+{
+Console.WriteLine (" {0} max", n);
+Console.WriteLine (" {0} min", m);
+}
+
+
+else 
+{
+Console.WriteLine (" {0} max", m);
+Console.WriteLine (" {0} min", n);
+}
--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Task 6
+Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+Console.WriteLine ("Это число четное");
+
+else
+
+Console.WriteLine ("Это число нечетное");
