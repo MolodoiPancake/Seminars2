8d0101e045031dc3c1fa31f6db3a9690e279049a
Gorik <gosha_qwerty@rambler.ru>
2022-12-05 16:14:21 +0800

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Task10
+Console.Write("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int x1 = x / 100;
+int x3 = x % 10;
+
+int x4 = (x - (x1 * 100 + x3));
+
+Console.WriteLine(x4 / 10);
+
