784bca9e25b76c41237930563d28c405ab342992
Gorik <gosha_qwerty@rambler.ru>
2022-12-05 16:12:33 +0800

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,6 @@
+﻿//Task2
+Console.Clear();
+Console.Write("Введите первое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int m = Convert.ToInt32(Console.ReadLine());
