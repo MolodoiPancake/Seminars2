22a98414900e48992679d5743e22a3e128729565
Gorik <gosha_qwerty@rambler.ru>
2022-12-05 16:13:23 +0800

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿//task4
+Console.Clear();
+Console.Write("Введите первое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(n > m && n > b)
+Console.WriteLine(n);
+else if (m > b)
+Console.WriteLine(m);
+else 
+Console.WriteLine(b);
