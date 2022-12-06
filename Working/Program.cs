c7ea0b561a7acb1ac369e5b6d18b6f63a6f95a6b
Gorik <gosha_qwerty@rambler.ru>
2022-12-06 15:38:57 +0800

    TASK 17

---
+++ Working/Program.cs
@@ -0,0 +1,21 @@
+﻿//task 17
+Console.Clear();
+Console.Write("Введите номер четверти ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+while (n > 4 || n < 1)
+{
+    Console.Write("Вы ошиблись!\nВведите номер четверти ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n == 1)
+Console.WriteLine("X > 0  Y > 0");
+else if (n == 2)
+Console.WriteLine("X < 0  Y > 0");
+else if (n == 3)
+Console.WriteLine("X < 0  Y < 0");
+else if (n == 4)
+Console.WriteLine("X > 0  Y < 0");
+
