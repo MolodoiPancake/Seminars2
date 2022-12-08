8407a2ba1141dc51ef2e92ce81d743f481fa0a98
Gorik <gosha_qwerty@rambler.ru>
2022-12-08 12:41:50 +0800

    Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine();
+
+Console.Write("Введите цифру дня недели ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1 || n > 8)
+Console.WriteLine("Вы ошиблись");
+
+else if (n == 1)
+Console.WriteLine("Сегодня не выходной");
+else if
+
+    (n == 2)
+    Console.WriteLine("Сегодня не выходной");
+
+else if
+
+    (n == 3)
+    Console.WriteLine("Сегодня не выходной");
+
+else if
+
+    (n == 4)
+    Console.WriteLine("Сегодня не выходной");
+
+else if 
+    (n == 5)
+    Console.WriteLine("Сегодня не выходной");
+
+else if 
+    (n == 6)
+    Console.WriteLine("Урааа, сегодня выходной!");
+
+else if 
+    (n == 7)
+    Console.WriteLine("Урааа, сегодня выходной!");
+
