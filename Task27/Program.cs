07ab38dfc08284fda70dcaf59bdb2d1a3afc6bc7
Gorik <gosha_qwerty@rambler.ru>
2022-12-10 18:03:22 +0800

    Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введте первое число ");
+int n = Convert.ToInt32(Console.ReadLine()); int i = 1, count = 0;
+while(i < n + 1)
+{
+    count = count + n % 10;
+    n = n / 10;
+
+}
+Console.WriteLine(count);
