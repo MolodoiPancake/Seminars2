272e08807cbe9f8b6cc2e1aa9e0a3642d0757f7f
Gorik <gosha_qwerty@rambler.ru>
2022-12-10 17:45:20 +0800

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Clear();
+Console.Write("Введте первое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введте второе число ");
+int m = Convert.ToInt32(Console.ReadLine()), count = n;
+
+for(int i = 1; i < m; i++)
+{   
+    n = n * count;
+    
+}
+Console.WriteLine(n);
--- Working/Program.cs
+++ Working/Program.cs
@@ -1,9 +1,13 @@
 ﻿//task 23
 Console.Clear();
-Console.Write("Введите число ");
+Console.Write("Введте первое число ");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введте второе число ");
+int m = Convert.ToInt32(Console.ReadLine()), count = n;
 
-for (int i = 1; i <= n; i++)
-
-Console.WriteLine(Math.Pow(i, 3));
-
+for(int i = 1; i < m; i++)
+{   
+    n = n * count;
+    
+}
+Console.WriteLine(n);
